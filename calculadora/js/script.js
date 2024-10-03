124a048dc4dcdcc3a29994a3fed0a4a2ec346bd2
Yukari16 <yukari.nicole.nunes@gmail.com>
2024-10-03 15:20:02 +0000

    Iniciado o JavaScript

---
+++ calculadora/js/script.js
@@ -0,0 +1,15 @@
+//Faz Referê aos elementos DOM
+//alert("Funcionou")
+const numero1 = document.querySelector ('#numero1')
+const numero2 = document.querySelector ('#numero2')
+const btnCalcular = document.querySelector ('#btn-calcular')
+const resultado = document.querySelector ('.resultado')
+const divisao = document.querySelector ('.divisao')
+const adicao = document.querySelector ('.adicao')
+const subtracao = document.querySelector ('.subtracao')
+const multiplicacao = document.querySelector ('.multipliacacao')
+//Criação a Função Calcular
+
+
+
+//Adiciona Esculta de Evento
