cc4446fe396e4abc38a98d6835eb09d18b60ec37
Yukari16 <yukari.nicole.nunes@gmail.com>
2024-10-17 15:23:17 +0000

    Concluimos os codigos do java

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -7,9 +7,34 @@ const resultado = document.querySelector ('.resultado')
 const divisao = document.querySelector ('.divisao')
 const adicao = document.querySelector ('.adicao')
 const subtracao = document.querySelector ('.subtracao')
-const multiplicacao = document.querySelector ('.multipliacacao')
+const multiplicacao = document.querySelector ('.multiplicacao')
 //Criação a Função Calcular
+function calcular(){
+    const n1 = Number(numero1.value)
+    const n2 = Number(numero2.value)
+    if(typeof n1 === 'number' && typeof n2 === 'number') {
 
+        const add = `O Resultado da Adição ${n1} + ${n2} = ${n1+n2}`
+        adicao.innerHTML = add
+        //alert(adicao)
 
+        const sub = `O Resultado da Subtração ${n1} - ${n2} = ${n1-n2}`
+        subtracao.innerHTML = sub
+        //alert(subtracao)
 
+        const mul = `O Resultado da Multiplicação ${n1} * ${n2} = ${n1*n2}`
+        multiplicacao.innerHTML = mul
+        //alert(multipliacacao)
+
+        const div = `O Resultado da Divisão ${n1} / ${n2} = ${n1/n2}`
+        divisao.innerHTML = div
+        //alert(divisao)
+    } else{
+        alert('Por favor, digeite um número legitimo')
+    }
+}
 //Adiciona Esculta de Evento
+btnCalcular.addEventListener('click', function (evento){
+    evento.preventDefault() //Tirar o comportamento padrão
+    calcular()
+})
